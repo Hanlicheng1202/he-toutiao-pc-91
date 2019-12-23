9a98be74b785e060eb076e2ec9fd7a4a0533c201
Hanlicheng1202 <935440759@qq.com>
2019-12-23 17:36:32 +0800

    完成登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import Login from '@/views/login'
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     // 路由规则
+    // 登录
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login }
   ]
 })
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container-login">
+    login
+    <el-card class="mycard">
+      <img src="../../assets/logo_index.png" alt />
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="tel" placeholder="请输入手机号码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="yanzhengma" placeholder="请输入验证码" style="width:240px;margin-right:20px"></el-input>
+          <el-button>发送短信</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="flag" @click="flag=!flag">我已经阅读和同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      loginForm: {},
+      tel: '',
+      yanzhengma: '',
+      flag: false
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.container-login {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: url(../../assets/login_bg.jpg) no-repeat center/cover;
+  .mycard {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  img {
+    display: block;
+    margin: 0 auto 30px;
+    height: 50px;
+  }
+}
+</style>
